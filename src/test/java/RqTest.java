b7152ec3f200f8c7446484d247914b1471897efd
chacha86 <taejincha0619@gmail.com>
2026-02-05 12:25:23 +0900

    t2 - r

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RqTest {
 
     @Test
-    @DisplayName(value = "rq.getActionName() - 삭제")
+    @DisplayName(value = "명령: 삭제?id=1 일때, rq.getActionName() - 삭제")
     void t1() {
 
         Rq rq = new Rq("삭제?id=1");
@@ -16,12 +16,21 @@ public class RqTest {
     }
 
     @Test
-    @DisplayName(value = "rq.getActionName() - 수정")
+    @DisplayName(value = "명령: 수정?id=1 일때, rq.getActionName() - 수정")
     void t2() {
 
         Rq rq = new Rq("수정?id=1");
         String actionName = rq.getActionName();
         assertThat(actionName).isEqualTo("수정");
 
+    }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keyword=자바 일때, rq.getParam(\"kewyord\") -> \"자바\"")
+    void t3() {
+
+        Rq rq = new Rq("목록?keyword=자바");
+        String rst = rq.getParam("keyword");
+        assertThat(rst).isEqualTo("자바");
     }
 }
